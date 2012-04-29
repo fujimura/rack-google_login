e9d3c2382b3ea4fda6e241944fdd453485e2b8ea
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-04-29 22:05:58 +0900

    Update example to use return_to, add docs

--- example/app.rb
+++ example/app.rb
@@ -4,12 +4,17 @@ enable :inline_templates
 
 before do
   if session['user_info'].nil? && request.path != '/login'
+    session['return_to'] = request.path
     redirect '/login'
   end
 end
 
 get '/' do
   erb :index
+end
+
+get '/foo' do
+  erb :foo
 end
 
 get '/login' do
@@ -20,6 +25,14 @@ get '/logout' do
   session['user_info'] = nil
   redirect '/'
 end
+
+=begin
+
+Rack::GoogleLogin catches request to /callback.
+It authorizes code, get user_info, set it to session, and redirect to root.
+if return_to was set in session, will be back to there.
+
+=end
 
 __END__
 
@@ -34,6 +47,16 @@ __END__
     <% end %>
     <p>
       <a href='/logout'>Logout</a>
+    </p>
+  </body>
+</html>
+
+@@ foo
+<html>
+  <body>
+    <h1>Bar</h1>
+    <p>
+      <a href='/'>back</a>
     </p>
   </body>
 </html>
